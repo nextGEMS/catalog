706ae9fefce9ce09e373e2b97e3fc7fbc1f42f21
Florian Ziemen <florianziemen@users.noreply.github.com>
2025-02-26 19:58:29 +0100

    working playground

--- scripts/intake_to_stac.py
+++ scripts/intake_to_stac.py
@@ -6,8 +6,12 @@ import yaml
 from collections import defaultdict
 from typing import Union
 from pathlib import Path
-import intake
 import intake_tools
+import logging
+
+logger = logging.getLogger("intake_to_stac")
+logging.basicConfig()
+logger.setLevel(logging.ERROR)
 
 
 class catalog_generator:
@@ -18,19 +22,20 @@ class catalog_generator:
         self.read_defaults()
         self.create_basic_catalog()
 
-    def read_defaults(self, source: Union[Path, str] = Path(__file__).parent/"defaults.yaml"):
+    def read_defaults(
+        self, source: Union[Path, str] = Path(__file__).parent / "defaults.yaml"
+    ):
         with open(source) as source_file:
             self.defaults = yaml.safe_load(source_file)
 
     def create_basic_catalog(self) -> None:
         self.catalog = Catalog(**self.defaults["catalog_properties"])
 
-    def parse_file(self, filename):
-        entries = intake.open_catalog(filename)
-        simulations = self._sort_by_simulation_id(entries)
-        for name, sim in simulations.items():
-            item = self._gen_item(name, sim[0])
-            self.catalog.add_item(item)
+    def parse_catalog(self, catalog):
+        intake_tools.traverse_tree(
+            catalog,
+            None,
+        )
 
     def _gen_item(self, name, sim):
         time_range = __class__.get_time_range(sim)
@@ -38,7 +43,6 @@ class catalog_generator:
             id=name,
             geometry=None,  # Replace with actual geometry, if available
             bbox=None,  # Replace with actual bounding box, if available
-            datetime=None,  # Replace with actual datetime, if applicable
             **time_range,
             properties=sim.describe()["metadata"],
         )
@@ -52,23 +56,101 @@ class catalog_generator:
 
     def _gen_asset(self, sim, params) -> Asset:
         asset_href = sim(**params).urlpath
+        __class__._check_href(sim, params, asset_href)
+        logger.debug(asset_href)
+        if asset_href.endswith (".nc"):
+            return self._gen_nc_asset(sim, params)
+        elif asset_href.endswith(".zarr") or asset_href.endswith(".parq") or asset_href.startswith("reference:"):
+            return self._gen_zarr_asset(sim, params)
+        else:
+            raise RuntimeError(f"what is this? {asset_href}")
+        
+    def _gen_nc_asset(self, sim, params):
+        asset_href = sim(**params).urlpath
         param_string = "_".join([str(x) for x in list(params.values())])
+        protocol = __class__._get_protocol(asset_href)
+        extra_fields = dict(
+            metadata=dict(roles=["data", "netcdf", protocol]),
+            xarray=dict(open_kwargs=dict(consolidated=True)),
+        )
+        asset = Asset(
+            href=asset_href,
+            media_type=MediaType.NETCDF,
+            title=param_string,
+            description=sim.metadata.get("description", "description"),
+            extra_fields=extra_fields,
+        )
+        return asset
+
+    def _get_protocol(asset_href):
+        protocol = asset_href.split(":")[0]
+        if protocol == asset_href:
+            protocol = "file"
+        return protocol
+
+    def _gen_zarr_asset(self, sim, params) -> Asset:
+        asset_href = str(sim(**params).urlpath)
+        param_string = "_".join([str(x) for x in list(params.values())])
+        protocol = __class__._get_protocol(asset_href)
+        extra_fields = dict(
+            metadata=dict(roles=["data", "zarr", protocol]),
+            xarray=dict(open_kwargs=dict(consolidated=True)),
+        )
         asset = Asset(
             href=asset_href,
             media_type=MediaType.ZARR,
             title=param_string,
-            description="md",
-            extra_fields=self.defaults["asset_properties"],
+            description="description",
+            extra_fields=extra_fields,
         )
         return asset
 
     @staticmethod
+    def _check_href(sim, params, href):
+        if isinstance(href, list):
+            raise RuntimeError(
+                f"URI for {sim(**params).name} is a list. Can only handle single files."
+            ) from None
+        # elif "*" in href:
+        #    raise RuntimeError("Found a * in the href for {sim(**params).name}. Needs to be a real file name: {href}") from None
+
+    @staticmethod
+    def query_metadata(entry, variants):
+        for variant in variants:
+            if variant in entry.metadata:
+                return entry.metadata[variant]
+
+    # default return is None
+
+    @staticmethod
     def get_time_range(entry):
-        ds = entry.to_dask()
+        start_datetime = __class__.query_metadata(
+            entry,
+            ("start_datetime", "start_time", "time_start", "date_start", "start_date"),
+        )
+        end_datetime = __class__.query_metadata(
+            entry, ("end_datetime", "end_time", "time_end", "date_end", "end_date")
+        )
+        if start_datetime is None or end_datetime is None:
+            try:
+                ds = entry.to_dask()
+                start_datetime = ds.time.values[0]
+                end_datetime = ds.time.values[-1]
+            except (KeyError, FileNotFoundError, IndexError):
+                logger.debug(
+                    f"Could not get time range for {entry.name} by loading it. Please supply 'start_datetime' and 'end_datetime' in the metadata.",
+                )
+                return dict(datetime=datetime.datetime.now())
+            except Exception as e:
+                logger.critical(
+                    f"Ran into {type(e)}: {e}, when trying to get start and end times for {entry.name}"
+                )
+                raise e
+
         convert_time = __class__.convert_time
         return dict(
-            start_datetime=convert_time(ds.time.values[0]),
-            end_datetime=convert_time(ds.time.values[-1]),
+            start_datetime=convert_time(start_datetime),
+            end_datetime=convert_time(end_datetime),
         )
 
     @staticmethod
--- scripts/intake_tools.py
+++ scripts/intake_tools.py
@@ -1,4 +1,5 @@
 import logging
+from collections.abc import Iterable
 
 logger = logging.getLogger("intake_tools")
 
@@ -22,3 +23,61 @@ def iterate_user_parameters(entry, fixed_keys=None):
             yield fixed_keys
         else:
             yield from iterate_user_parameters(entry, fixed_keys.copy())
+
+
+def warn_esm_cat(cat, child, position):
+    logging.warning(
+        f"skipping {'.'.join(position)}.{child}, as it seems to be an intake-esm catalog"
+    )
+
+
+def traverse_tree(
+    cat,
+    subcat_callback,
+    entry_callback,
+    esm_cat_callback=warn_esm_cat,
+    levels=0,
+    position=list(),
+):
+    """Traverses an intake tree and call a function on everything it finds in it.
+    subcat_callback is called on anything iterable (should be sub-catalogs)
+    entry_callback is called on anything not iterable (should be datasets)
+    esm_cat_callback will be called on intake_esm_catalogs. Defaults to a warning message, as loading them can consume a lot of memory and time.
+    """
+    if levels and (levels - 1 < len(position)):
+        return
+    for child in list(cat):
+        logging.debug(f"processing {child}")
+        if detect_esm_cat(cat, child, position):
+            esm_cat_callback(cat, child, position)
+            continue
+        try:
+            cat[child]
+        except FileNotFoundError as missing:
+            logging.critical(
+                f"Error processing {'.'.join(position)}.{child}: File not found: {missing}"
+            )
+            continue
+
+        if isinstance(cat[child], Iterable):
+            subcat_callback(cat, child, position)
+            traverse_tree(
+                cat[child],
+                subcat_callback,
+                entry_callback,
+                levels=levels,
+                position=position + [child],
+            )
+        else:
+            entry_callback(cat, child, position)
+
+
+def detect_esm_cat(cat, child, position):
+    try:
+        if "esm_datastore" in str(cat._entries[child]._driver):
+            return True
+    except Exception as e:
+        logging.error(
+            f"Can't really decide the type of \n{position}{child}\nran into {e}"
+        )
+    return False
